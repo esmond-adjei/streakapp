2a1077176b7e5848bdc8b5a6ec89a6e4b3fc2438
Esmond Adjei <xmongram@gmail.com>
2024-01-20 20:53:54 +0000

    update: beacon feature

--- app/components/search.js
+++ app/components/search.js
@@ -4,9 +4,9 @@ import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
 export default function Search() {
     return (
         <div className='flex justify-between items-center gap-2'>
-        <div className="flex-1 flex items-center justify-between p-2 h-[40px] bg-white rounded-lg">
+        <div className="flex-1 flex items-center p-2 h-[40px] bg-white rounded-lg">
           <FontAwesomeIcon icon={faSearch} className='text-blue-500 p-2 rounded-lg'/>
-          <input type="search" placeholder="Search" className="border-none outline-none text-sm text-gray-600 p-2" />
+          <input type="search" placeholder="Search" className="border-none outline-none text-sm text-gray-600 py-2" />
         </div>
         {/* filter */}
         <div className='shrink-0 p-2 h-[40px] text-blue-500 bg-white hover:bg-blue-500 hover:text-white rounded-lg'>
